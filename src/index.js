81f57ecb24069658ecdf564b27076c706c9c9f62
cynthia <isa.cinty@gmail.com>
2025-06-28 17:07:23 +0100

    commit message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './global.css'; // Import global CSS file
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
